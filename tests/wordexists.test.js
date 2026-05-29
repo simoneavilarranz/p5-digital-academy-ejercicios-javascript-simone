d9842de320fa6e76dd2d939ea8b6da6d9d9e04a6
unknown <avilarranz@gmail.com>
2026-05-29 12:58:30 +0200

    feat:realizado primer test true exitoso

--- tests/wordexists.test.js
+++ tests/wordexists.test.js
@@ -10,11 +10,4 @@ describe("English", () => {
         expect(result).toBe(expected);
     })
 
-    test("should return false", () => {
-        const input = "abcnEglishsef";
-        const expected = false;
-        const result = english(input);
-        expect(result).toBe(expected);
-    })
-
 })
